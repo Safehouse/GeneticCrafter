31cfe1c834c587af5add6d54559d5cbc2fa42895
Michael Senn <m.senn93@gmail.com>
2012-01-27 22:23:33 +0100

    Changed Gene to use byte instead of Allele

--- GeneticCrafter/Gene.vb
+++ GeneticCrafter/Gene.vb
@@ -4,7 +4,44 @@
 
 #Region "Properties"
 
+        Public Property Name As String
 
+            Get
+                Return _Name
+            End Get
+
+            Set(value As String)
+                _Name = value
+            End Set
+
+        End Property
+        Private _Name As String
+
+        Private Property byte1 As Byte
+
+            Get
+                Return _byte1
+            End Get
+
+            Set(value As Byte)
+                _byte1 = value
+            End Set
+
+        End Property
+        Private _byte1 As Byte
+
+        Private Property byte2 As Byte
+
+            Get
+                Return _byte2
+            End Get
+
+            Set(value As Byte)
+                _byte2 = value
+            End Set
+
+        End Property
+        Private _byte2 As Byte
 
 #End Region
 
